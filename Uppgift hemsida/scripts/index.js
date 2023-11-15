f8875c7f25e50c04573a928f37e947e33f565c22
heridfel76 <mats.johansson.syne23@edu.tucsweden.se>
2023-11-15 11:58:16 +0100

    Lagt till kommentarer i javascript

--- Uppgift hemsida/scripts/index.js
+++ Uppgift hemsida/scripts/index.js
@@ -1,10 +1,10 @@
 // Konstanter
-const movieTemplate = document.querySelector('[movieTemplate]');        // Konstant för template som ska användas för att klona.
+const movieTemplate = document.querySelector('[movieTemplate]');        // Konstant för template som ska användas som mall för kloning.
 const movieCollections = document.querySelector('[movieCollections]');  // Konstant för div som template-kloner ska placers i.
 const movieSearch = document.querySelector('[movieSearch]');            // Konstant för sökruta
-const parsedMovies = JSON.parse(localStorage.getItem('movies'));        // Konstant för filmer. Hämtar sträng från localStorage och konverterar till object.
+const parsedMovies = JSON.parse(localStorage.getItem('movies'));        // Konstant för filmer. Hämtar sträng från localStorage och konverterar till array av object.
 
-// Variabel, tom array. Byggs på nedan i programmet med nya film-objekt.
+// Variabel, tom array. Byggs på nedan i programmet med film-objekt, om de finns upplagda.
 let movies = [];                                                        
 
 // Lyssna på input i sökruta. Göm filmer som inte innehåller, eller matcher, input. 
@@ -14,7 +14,7 @@ movieSearch.addEventListener("input", (e) => {
     // För varje "film"-objekt i arryren movies, kontrollera om input i sökruta matchar med någon text i titel eller beskrivning.
     movies.forEach(movie => {
 
-        // Konstant som sann eller falsk beroende på om input i sökruta finns med i filmens titel eller beskrivning.
+        // Konstant som är sann eller falsk beroende på om input i sökruta finns med i filmens titel eller beskrivning.
         const isVisible = 
             movie.title.toLowerCase().includes(value) ||
             movie.description.toLowerCase().includes(value); 
@@ -27,15 +27,13 @@ movieSearch.addEventListener("input", (e) => {
 // Foreach-loop som lägger in titel, beskrivning och åldersgräns från objekten (från localStorage) i arrayen movies.
 // Template klonas med innehåll och lägger sig som en div under div:en [movieCollections] också.
 movies = parsedMovies.map(movie => {                                            
-    const movieNewDiv = movieTemplate.content.cloneNode(true).children[0];      // Konstant för template som ska klonas. Children 0 för att "peka" på första diven i template.
-    const title = movieNewDiv.querySelector('[title]');                         // Konstant för div [title] i template.
-    const description = movieNewDiv.querySelector('[description]');             // Konstant för div [description] i template.
-    const age_limit = movieNewDiv.querySelector('[age_limit]');                 // Konstant för div [age_limit] i template.
-    title.textContent = movie.title;                                            // Lägger till text, titel från localStorage, i div [title] i template.
-    description.textContent = movie.description;                                // Lägger till text, beskrivning från localStorage, i div [description] i template.
-    age_limit.textContent = "Åldersgräns: " + movie.age_limit;                  // Lägger till text. åldersgräns från localStorage, i div [age_limit] i template.
-    movieCollections.append(movieNewDiv);                                       // Klonar template till ny div under div "movieCollections".
-    
-    // Returnerar object med titel och beskrivning till arrayen movies.
-    return { title: movie.title, description: movie.description, element: movieNewDiv }
+    const movieNewDiv = movieTemplate.content.cloneNode(true).children[0];              // Konstant för template som ska klonas. Children 0 för att "peka" på första diven i template.
+    const title = movieNewDiv.querySelector('[title]');                                 // Konstant för div [title] i template.
+    const description = movieNewDiv.querySelector('[description]');                     // Konstant för div [description] i template.
+    const age_limit = movieNewDiv.querySelector('[age_limit]');                         // Konstant för div [age_limit] i template.
+    title.textContent = movie.title;                                                    // Lägger till text, titel från localStorage, i div [title] i template.
+    description.textContent = movie.description;                                        // Lägger till text, beskrivning från localStorage, i div [description] i template.
+    age_limit.textContent = "Åldersgräns: " + movie.age_limit;                          // Lägger till text. åldersgräns från localStorage, i div [age_limit] i template.
+    movieCollections.append(movieNewDiv);                                               // Klonar template med ifylld text till ny div som läggs under div "movieCollections".
+    return { title: movie.title, description: movie.description, element: movieNewDiv } // Returnerar titel och beskrivning från "ifylld" template till arrayen movies.
 });
--- Uppgift hemsida/scripts/inmatning.js
+++ Uppgift hemsida/scripts/inmatning.js
@@ -1,4 +1,4 @@
-// Lyssnar på knapptryck från inmatning.html
+// Lyssnar på knapptryck
 document.getElementById('buttonSave').addEventListener('click', saveMovie);
 document.getElementById('buttonList').addEventListener('click', showMovies);
 document.getElementById('buttonDelete').addEventListener('click', deleteMovies);
@@ -6,40 +6,50 @@ document.getElementById('buttonDelete').addEventListener('click', deleteMovies);
 
 // Funktioner //
 
-//Spara film till localStorage
+//Spara film till localStorage.
 function saveMovie() {
 
-    // Skapar en variabel som är ett objekt (titel, beskrivning, åldersgräns)
-    let newMovie = {
-        "title": document.getElementById('inputTitle').value,
-        "description": document.getElementById('inputDescription').value,
-        "age_limit": document.getElementById('inputAge').value,
+    // Konstanter för de tre fälten.
+    const title = document.getElementById('inputTitle').value;
+    const description = document.getElementById('inputDescription').value;
+    const age_limit = document.getElementById('inputAge').value;
+
+    // If-sats för att inget fält lämnas tomt. Annars spara till localStorage.
+    if (title == "" || description == "" || age_limit == "") {
+        alert('Alla tre fälten ovan måste vara ifyllda för att kunna spara filmen.');
     }
+    else {
+        // Skapar en variabel som är ett objekt (titel, beskrivning, åldersgräns)
+        let newMovie = {
+            "title": title,
+            "description": description,
+            "age_limit": age_limit,
+        }
 
-    //  Hämtar data från localStorage, JSON för att omvandla från sträng till objekt, och tillbaka från objekt till sträng
-    let movies = localStorage.getItem('movies');              // Hämtar sträng från localStorage till variabel movies
-    movies = movies ? JSON.parse(movies) : [];                // Gör om sträng till objekt
-    movies.push(newMovie);                                    // Lägg till nytt objekt i variabeln movies
-    localStorage.setItem('movies', JSON.stringify(movies));   // Gör om variabeln movies till en sträng och skicka tillbaka localstorage
+        // Hämtar data från localStorage, JSON för att omvandla från sträng till array av object, och tillbaka till sträng
+        let movies = localStorage.getItem('movies');              // Hämtar sträng från localStorage till variabel movies
+        movies = movies ? JSON.parse(movies) : [];                // Gör om sträng till array av objekt, eller tom array om inget finns sparat sedan tidigare
+        movies.push(newMovie);                                    // Lägg till nytt objekt i array
+        localStorage.setItem('movies', JSON.stringify(movies));   // Gör om variabeln movies till en sträng och skicka tillbaka localstorage
 
-    // Rensar värden i de tre input-fälten
-    document.getElementById('inputTitle').value = '';
-    document.getElementById('inputDescription').value = '';
-    document.getElementById('inputAge').value = '';
+        // Rensar värden i de tre input-fälten
+        document.getElementById('inputTitle').value = '';
+        document.getElementById('inputDescription').value = '';
+        document.getElementById('inputAge').value = '';
 
-    // Meddelande till användare
-    alert('Film sparad!');
-
+        // Meddelande till användare
+        alert('Film sparad!');
+    }
 }
 
-// Visar filmer sparade i localStorage om det finns
+// Visar filmer sparade i localStorage om det finns några filmer
 function showMovies() {
     const movies = localStorage.getItem('movies');
     if (movies) {
         const parsedMovies = JSON.parse(movies);
         if (parsedMovies.length > 0) {
-            parsedMovies.forEach(element => {
-                alert('Title: ' + element.title + '\nDescription: ' + element.description + '\nAge limit: ' + element.age_limit);
+            parsedMovies.forEach(movie => {
+                alert('Title: ' + movie.title + '\nDescription: ' + movie.description + '\nAge limit: ' + movie.age_limit);
             });
 
         }
@@ -53,12 +63,12 @@ function showMovies() {
 }
 
 function deleteMovies() {
-    let text = "Vill du verkligen radera alla tv-program?\n\nTryck på OK för att radera.\nTryck på avbryt för att ångra.";
+    let text = "Vill du verkligen radera alla filmer?\n\nTryck på OK för att radera.\nTryck på avbryt för att ångra.";
     if (confirm(text) == true) {
         localStorage.removeItem('movies');
-        alert('Alla tv-program raderade!');
+        alert('Alla filmer raderades!');
     }
     else {
-        alert('Åtgärden avbröts! (inga tv-program raderade)');
+        alert('Åtgärden avbröts! Inga filmer raderades.');
     }
 }
